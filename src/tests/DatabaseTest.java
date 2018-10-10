bf7a7f10b7ded4dc0e6598f53a026b9de90db060
git <julian@192.168.1.40>
2018-10-10 09:14:05 +0200

    socket test

--- src/tests/DatabaseTest.java
+++ src/tests/DatabaseTest.java
@@ -1,5 +1,8 @@
 package tests;
 
+import java.io.IOException;
+import java.net.Socket;
+
 /**
  *
  * @author julian
@@ -11,8 +14,28 @@ public class DatabaseTest {
         Class.forName("org.postgresql.Driver");
     }
     
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void checkServer() throws IOException {
         
-        loadDriver();
+        Socket s = new Socket("127.0.0.1", 5432);
+        s.close();
+    }
+    
+    public static void main(String[] args) {
+        
+        try {
+            loadDriver();
+        } catch (ClassNotFoundException ex) {
+            System.out.println("driver is not available");
+            ex.printStackTrace();
+            return;
+        }
+        
+        try {
+            checkServer();
+        } catch (IOException ex) {
+            System.out.println("server is not running");
+            ex.printStackTrace();
+            return;
+        }
     }
 }
