62764a8e60648f03f428439cf7b3c5e620c232cf
git <julian@192.168.1.40>
2018-10-26 11:58:31 +0200

    print SQLState / codes

--- src/tests/TransactionTest.java
+++ src/tests/TransactionTest.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.SQLWarning;
 import java.sql.Statement;
 import java.util.Properties;
 
@@ -135,6 +136,63 @@ public class TransactionTest {
 
         } finally {
             conn.setAutoCommit(true);
+        }
+    }
+    
+    // PRINT SQL EXCEPTIONS INFO
+    
+    public static void printSQLException(SQLException ex) {
+
+        for (Throwable e : ex) {
+            if (e instanceof SQLException) {
+                if (ignoreSQLException(((SQLException) e).getSQLState()) == false) {
+
+                    e.printStackTrace(System.err);
+                    System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+                    System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+                    System.err.println("Message: " + e.getMessage());
+
+                    Throwable t = ex.getCause();
+                    while (t != null) {
+                        System.out.println("Cause: " + t);
+                        t = t.getCause();
+                    }
+                }
+            }
+        }
+    }
+
+    public static boolean ignoreSQLException(String sqlState) {
+
+        // add any checks here: if (sqlState.equalsIgnoreCase("X0Y32")) ...         
+        return false;
+    }
+    
+    // PRINT WARNINGS
+    
+    public static void getWarningsFromResultSet(ResultSet rs)
+            throws SQLException {
+        printWarnings(rs.getWarnings());
+    }
+
+    public static void getWarningsFromStatement(Statement stmt)
+            throws SQLException {
+        printWarnings(stmt.getWarnings());
+    }
+
+    public static void printWarnings(SQLWarning warning) {
+
+        if (warning != null) {
+            System.out.println("\n---Warning---\n");
+
+            while (warning != null) {
+                System.out.println("Message: " + warning.getMessage());
+                System.out.println("SQLState: " + warning.getSQLState());
+                System.out.print("Vendor error code: ");
+                System.out.println(warning.getErrorCode());
+                System.out.println("");
+                warning = warning.getNextWarning();
+            }
         }
     }
 }
