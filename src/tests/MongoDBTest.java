b7ae29d6db975882c5a780c4bd3ffc46969e20d9
git <julian@192.168.1.40>
2018-11-22 12:10:35 +0100

    mongo test

---
+++ src/tests/MongoDBTest.java
@@ -0,0 +1,17 @@
+package tests;
+
+//import com.mongodb.client.MongoClient;
+//import com.mongodb.client.MongoDatabase;
+
+/**
+ *
+ * @author julian
+ */
+public class MongoDBTest {
+    
+    public static void main(String[] args) {
+        
+        //MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+        //MongoDatabase database = mongoClient.getDatabase("test");
+    }
+}
