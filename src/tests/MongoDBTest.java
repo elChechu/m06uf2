8c75a0b91a2e42c272f66d037045daa90b79ad5b
julian <julian@DESKTOP-JFUCUC8>
2018-11-22 18:46:55 +0100

    testing code

--- src/tests/MongoDBTest.java
+++ src/tests/MongoDBTest.java
@@ -1,7 +1,13 @@
 package tests;
 
-//import com.mongodb.client.MongoClient;
-//import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import java.util.Map.Entry;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import persones.Persona;
 
 /**
  *
@@ -11,7 +17,24 @@ public class MongoDBTest {
     
     public static void main(String[] args) {
         
-        //MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
-        //MongoDatabase database = mongoClient.getDatabase("test");
+        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+        MongoDatabase database = mongoClient.getDatabase("testdb");
+        
+        MongoCollection<Document> collection = database.getCollection("persones");
+        
+        Document doc = collection.find().first();
+        
+        for (Entry<String, Object> entry: doc.entrySet()) {
+            System.out.println(
+                    entry.getKey() + " = " + entry.getValue());
+        }
+        
+        String nom = (String) doc.get("nom");
+        int edat = ((Number) doc.get("edat")).intValue();
+        ObjectId id = doc.getObjectId("_id");
+        String idStr = id.toString();
+        
+        Persona p = new Persona(idStr, nom, edat);
+        System.out.println(p);
     }
 }
